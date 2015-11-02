c23c502ff79109f5320dbf0b83b6c336e4a251c0
Paul Leo <paul.r.leo@gmail.com>
2015-11-02 00:38:58 +0000

    Initial Commit

---
+++ Badminton WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,421 @@
+//
+//  InterfaceController.swift
+//  Badminton WatchKit Extension
+//
+//  Created by Paul Leo on 25/10/2015.
+//  Copyright © 2015 TapDigital Ltd. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import HealthKit
+
+enum TDState {
+    case ReadyToBegin
+    case Started
+    case Ended
+}
+
+
+class InterfaceController: WKInterfaceController, TDWorkoutSessionManagerDelegate {
+    @IBOutlet var myMinusBtn: WKInterfaceButton!
+    @IBOutlet var myPlusBtn: WKInterfaceButton!
+    @IBOutlet var themMinusBtn: WKInterfaceButton!
+    @IBOutlet var themPlusBtn: WKInterfaceButton!
+    @IBOutlet var stateLbl: WKInterfaceLabel!
+    @IBOutlet var scoreLbl: WKInterfaceLabel!
+    @IBOutlet var heartRateLbl: WKInterfaceLabel!
+    @IBOutlet var mySetScoreLbl: WKInterfaceLabel!
+    @IBOutlet var themSetScoreLbl: WKInterfaceLabel!
+    @IBOutlet var heartIV: WKInterfaceImage!
+    
+    var canSaveGame = false
+    var canSaveMatch = false
+    
+    var overallScore : [[Int]] = [[0,0], [0,0], [0,0]]
+    var currentSet : Int = 0
+    var isMySaveActive = false
+    var isThemSaveActive = false
+    var currentState = TDState.ReadyToBegin
+    
+    let healthStore = HKHealthStore()
+    
+    var sessionContext: TDWorkoutSessionContext?
+    var workoutManager : TDWorkoutSessionManager?
+    
+    private var _timer: NSTimer?
+//    private var _ticks: Double = 0.0
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        setScoreString()
+        
+        guard HKHealthStore.isHealthDataAvailable() == true else {
+            displayNotAllowed()
+            return
+        }
+        
+        guard let heartRateType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate) else {
+            displayNotAllowed()
+            return
+        }
+        
+        guard let stepType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount) else {
+            displayNotAllowed()
+            return
+        }
+        
+        guard let eneryType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned) else {
+            displayNotAllowed()
+            return
+        }
+        
+        guard let distanceType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning) else {
+            displayNotAllowed()
+            return
+        }
+        
+        let typesToShare = Set([HKObjectType.workoutType()])        
+        let dataTypes = Set([heartRateType,stepType,eneryType,distanceType])
+        healthStore.requestAuthorizationToShareTypes(typesToShare, readTypes: dataTypes) { (success, error) -> Void in
+            if success {
+
+            } else {
+                self.displayNotAllowed()
+            }
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+        }
+
+        resetMenuItems()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+        
+        _timer?.invalidate()
+        _timer = nil
+    }
+    
+    func resetMenuItems() {
+        self.clearAllMenuItems()
+        self.addMenuItemWithItemIcon(WKMenuItemIcon.Repeat, title: "Reset Score", action: Selector("resetMenuPressed"))
+        switch currentState {
+        case .ReadyToBegin:
+            self.addMenuItemWithItemIcon(WKMenuItemIcon.Play, title: "Start", action: Selector("startBtnPressed"))
+            break
+        case .Started:
+            self.addMenuItemWithItemIcon(WKMenuItemIcon.Decline, title: "End", action: Selector("endBtnPressed"))
+            break
+        case .Ended:
+            self.addMenuItemWithItemIcon(WKMenuItemIcon.Play, title: "Start", action: Selector("startBtnPressed"))
+            break
+            
+        }
+    }
+    
+    //MARK: Menu Button events
+    
+    @IBAction func startBtnPressed() {
+        if self.sessionContext == nil || self.workoutManager == nil {
+            self.sessionContext = TDWorkoutSessionContext(healthStore: self.healthStore, activityType: .Badminton, locationType: .Indoor)
+            self.workoutManager = TDWorkoutSessionManager(context: self.sessionContext!)
+            self.workoutManager?.delegate = self
+        }
+        
+        workoutManager?.startWorkout()
+        _timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("timerTick"), userInfo: nil, repeats: true)
+        currentState = .Started
+        stateLbl.setText("Measuring...")
+        
+        resetMenuItems()
+    }
+    
+    @IBAction func endBtnPressed() {
+        workoutManager?.stopWorkoutAndSave()
+        currentState = .ReadyToBegin
+        stateLbl.setText("Ready")
+
+        resetMenuItems()
+        
+        _timer?.invalidate()
+        _timer = nil
+    }
+    
+    
+    @IBAction func resetMenuPressed() {
+        reset()
+    }
+    
+    //MARK: Button events
+    
+    @IBAction func myMinusBtnPressed() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let currentSetScore = self.overallScore[self.currentSet]
+            let mySetScore = currentSetScore[0]
+            let themSetScore = currentSetScore[1]
+
+            if mySetScore == 0 {
+                return
+            }
+
+            let newMySetScore = mySetScore - 1
+            self.overallScore[self.currentSet][0] = newMySetScore
+            self.mySetScoreLbl.setText("\(newMySetScore)")
+            
+            self.updateVisualState(newMySetScore, themSetScore: themSetScore)
+        });
+
+    }
+
+    
+    @IBAction func themMinusBtnPressed() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let currentSetScore = self.overallScore[self.currentSet]
+            let mySetScore = currentSetScore[0]
+            let themSetScore = currentSetScore[1]
+
+            if themSetScore == 0 {
+                return
+            }
+            
+            let newMySetScore = themSetScore - 1
+            self.overallScore[self.currentSet][1] = newMySetScore
+            self.themSetScoreLbl.setText("\(newMySetScore)")
+            
+            self.updateVisualState(mySetScore, themSetScore: newMySetScore)
+        });
+    }
+    
+    @IBAction func myPlusBtnPressed() {
+        dispatch_async(dispatch_get_main_queue(), {
+            if self.isMySaveActive {
+                self.saveScore()
+                return
+            }
+            
+            let currentSetScore = self.overallScore[self.currentSet]
+            let mySetScore = currentSetScore[0]
+            let themSetScore = currentSetScore[1]
+            
+            if self.isValidScore(mySetScore, themSetScore: themSetScore) { // won
+                let newMySetScore = mySetScore + 1
+                self.overallScore[self.currentSet][0] = newMySetScore
+                self.mySetScoreLbl.setText("\(newMySetScore)")
+                
+                self.updateVisualState(newMySetScore, themSetScore: themSetScore)
+            }
+        });
+    }
+
+    
+    @IBAction func themPlusBtnPressed() {
+        dispatch_async(dispatch_get_main_queue(), {
+            if self.isThemSaveActive {
+                self.saveScore()
+                return
+            }
+            
+            let currentSetScore = self.overallScore[self.currentSet]
+            let mySetScore = currentSetScore[0]
+            let themSetScore = currentSetScore[1]
+            
+            if self.isValidScore(mySetScore, themSetScore: themSetScore) { // won
+                let newMySetScore = themSetScore + 1
+                self.overallScore[self.currentSet][1] = newMySetScore
+                self.themSetScoreLbl.setText("\(newMySetScore)")
+                
+                self.updateVisualState(mySetScore, themSetScore: newMySetScore)
+            }
+        });
+    }
+
+    
+    //MARK: validation
+    
+    func isValidScore(mySetScore: Int, themSetScore: Int) -> Bool {
+        if mySetScore > themSetScore {
+            let difference = mySetScore - themSetScore
+            
+            if (mySetScore >= 21 && difference > 1) || mySetScore == 30 { // won
+                return false
+            }
+        } else {
+            let difference = themSetScore - mySetScore
+            
+            if (themSetScore >= 21 && difference > 1) || themSetScore == 30 { // won
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func updateVisualState(mySetScore: Int, themSetScore: Int) {
+        setScoreString()
+        
+        if mySetScore > themSetScore {
+            let difference = mySetScore - themSetScore
+
+            if (mySetScore >= 21 && difference > 1) || mySetScore == 30 { // won
+                showSaveSet(true)
+                return
+            }
+        } else {
+            let difference = themSetScore - mySetScore
+            
+            if (themSetScore >= 21 && difference > 1) || themSetScore == 30 { // won
+                // set save btn
+                showSaveSet(false)
+                return
+            }
+        }
+        
+        hideSaveSet()        
+    }
+
+    func showSaveSet(isMeWon: Bool) {
+        if isMeWon {
+            myPlusBtn.setBackgroundImageNamed("acceptBtn")
+            isMySaveActive = true
+        } else {
+            themPlusBtn.setBackgroundImageNamed("acceptBtn")
+            isThemSaveActive = true
+        }
+        
+        if currentSet == 2 {
+            canSaveMatch = true
+        } else {
+            canSaveGame = true
+        }
+    }
+    
+    func hideSaveSet() {
+        myPlusBtn.setBackgroundImageNamed("cockUpBtn")
+        themPlusBtn.setBackgroundImageNamed("cockUpBtn")
+        
+        isMySaveActive = false
+        isThemSaveActive = false
+    }
+    
+    func saveScore() {
+        if currentSet < 2 {
+            currentSet++
+        }
+        
+        mySetScoreLbl.setText("\(0)")
+        themSetScoreLbl.setText("\(0)")
+
+        hideSaveSet()
+        
+        isMySaveActive = false
+        isThemSaveActive = false
+        
+        setScoreString()
+    }
+    
+    func setScoreString() {
+        if currentSet == 0 {
+            scoreLbl.setText("Score: \(overallScore[0][0]) \(overallScore[0][1])")
+        } else if currentSet == 1 {
+            scoreLbl.setText("\(overallScore[0][0]) \(overallScore[0][1]), \(overallScore[1][0]) \(overallScore[1][1])")
+        } else {
+            scoreLbl.setText("\(overallScore[0][0]) \(overallScore[0][1]), \(overallScore[1][0]) \(overallScore[1][1]), \(overallScore[2][0]) \(overallScore[2][1])")
+        }
+    }
+    
+    func reset() {
+        mySetScoreLbl.setText("\(0)")
+        themSetScoreLbl.setText("\(0)")
+        
+        hideSaveSet()
+        
+        isMySaveActive = false
+        isThemSaveActive = false
+        
+        currentSet = 0
+        overallScore = [[0,0], [0,0], [0,0]]
+        setScoreString()
+    }
+    
+    func displayNotAllowed() {
+        heartRateLbl.setText("n/a")
+    }
+    
+    
+    func animateHeart() {
+        self.animateWithDuration(0.2) {
+            self.heartIV.setWidth(28)
+            self.heartIV.setHeight(28)
+        }
+        
+        let when = dispatch_time(DISPATCH_TIME_NOW, Int64(0.2 * double_t(NSEC_PER_SEC)))
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_after(when, queue) {
+            dispatch_async(dispatch_get_main_queue(), {
+                self.animateWithDuration(0.2, animations: {
+                    self.heartIV.setWidth(20)
+                    self.heartIV.setHeight(20)
+                })
+            })
+        }
+    }
+    
+    func timerTick() {
+        // Timers are not guaranteed to tick at the nominal rate specified, so this isn't technically accurate.
+        // However, this is just an example to demonstrate how to stop some ongoing activity, so we can live with that inaccuracy.
+        if let startDate = self.workoutManager?.workoutStartDate {
+            let now = NSDate()
+            let interval = now.timeIntervalSinceDate(startDate)
+            let seconds = fmod(interval, 60.0)
+            let minutes = fmod(trunc(interval / 60.0), 60.0)
+            let hours = trunc(interval / 3600.0)
+            let clockString = String.localizedStringWithFormat("%02.0f:%02.0f:%02.0f", hours, minutes, seconds)
+            self.stateLbl.setText(clockString)
+        }
+    }
+
+    // MARK: TDWorkoutSessionManagerDelegate
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didStartWorkoutWithDate startDate: NSDate) {
+        currentState = .Started
+        resetMenuItems()
+    }
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didStopWorkoutWithDate endDate: NSDate) {
+        currentState = .ReadyToBegin
+        resetMenuItems()
+    }
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateEnergyQuantity energyQuantity: HKQuantity) {
+        
+    }
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateDistanceQuantity distanceQuantity: HKQuantity) {
+        
+    }
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateStepQuantity stepQuantity: HKQuantity) {
+        
+    }
+    
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateHeartRateSample heartRateSample: HKQuantitySample) {
+        let value = heartRateSample.quantity.doubleValueForUnit(self.workoutManager!.countPerMinuteUnit)
+        self.heartRateLbl.setText(String(UInt16(value)))
+        self.animateHeart()
+        
+        timerTick()
+        
+        if _timer == nil {
+            _timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("timerTick"), userInfo: nil, repeats: true)
+        }
+    }
+}
---
+++ Badminton WatchKit Extension/TDWorkoutManager.swift
@@ -0,0 +1,308 @@
+//
+//  TDWorkoutManager.swift
+//  Badminton
+//
+//  Created by Paul Leo on 01/11/2015.
+//  Copyright © 2015 TapDigital Ltd. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class TDWorkoutSessionContext {
+    let healthStore : HKHealthStore
+    var activityType : HKWorkoutActivityType
+    var locationType : HKWorkoutSessionLocationType
+    
+    init(healthStore: HKHealthStore, activityType : HKWorkoutActivityType = .Other, locationType : HKWorkoutSessionLocationType = .Unknown) {
+        self.healthStore = healthStore
+        self.activityType = activityType
+        self.locationType = locationType
+    }
+}
+
+protocol TDWorkoutSessionManagerDelegate: class {
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didStartWorkoutWithDate startDate: NSDate)
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didStopWorkoutWithDate endDate: NSDate)
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateEnergyQuantity energyQuantity: HKQuantity)
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateDistanceQuantity distanceQuantity: HKQuantity)
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateStepQuantity stepQuantity: HKQuantity)
+    func workoutSessionManager(workoutSessionManager: TDWorkoutSessionManager, didUpdateHeartRateSample heartRateSample: HKQuantitySample)
+}
+
+
+class TDWorkoutSessionManager: NSObject, HKWorkoutSessionDelegate {
+    let healthStore : HKHealthStore
+    let workoutSession: HKWorkoutSession
+    
+    var workoutStartDate: NSDate?
+    var workoutEndDate: NSDate?
+    
+    var queries: [HKQuery] = []
+    var energySamples: [HKQuantitySample] = []
+    var distanceSamples: [HKQuantitySample] = []
+    var stepSamples: [HKQuantitySample] = []
+    var heartRateSamples: [HKQuantitySample] = []
+    
+    let energyUnit = HKUnit.calorieUnit()
+    let distanceUnit = HKUnit.meterUnit()
+    let stepUnit = HKUnit.countUnit()
+    let countPerMinuteUnit = HKUnit(fromString: "count/min")
+
+    let energyType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned)!
+    let distanceType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)!
+    let stepType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)!
+    let heartRateType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)!
+    
+    var currentEnergyQuantity: HKQuantity {
+        get {
+            var total : Double = 0.0
+            for quant in energySamples {
+                total += quant.quantity.doubleValueForUnit(energyUnit)
+            }
+            return HKQuantity(unit: energyUnit, doubleValue: total)
+        }
+    }
+    
+    var currentDistanceQuantity: HKQuantity {
+        get {
+            var total : Double = 0.0
+            for quant in distanceSamples {
+                total += quant.quantity.doubleValueForUnit(distanceUnit)
+            }
+            return HKQuantity(unit: distanceUnit, doubleValue: total)
+        }
+    }
+    
+    var currentStepQuantity: HKQuantity {
+        get {
+            var total : Double = 0.0
+            for quant in stepSamples {
+                total += quant.quantity.doubleValueForUnit(stepUnit)
+            }
+            return HKQuantity(unit: stepUnit, doubleValue: total)
+        }
+    }
+    
+    var currentHeartRateSample: HKQuantitySample?
+    
+    weak var delegate: TDWorkoutSessionManagerDelegate?
+    
+    init(context: TDWorkoutSessionContext) {
+        self.healthStore = context.healthStore
+        self.workoutSession = HKWorkoutSession(activityType: context.activityType, locationType: context.locationType)
+        
+        super.init()
+        
+        self.workoutSession.delegate = self
+    }
+    
+    func startWorkout() {
+        self.healthStore.startWorkoutSession(self.workoutSession)
+    }
+    
+    func stopWorkoutAndSave() {
+        self.healthStore.endWorkoutSession(self.workoutSession)
+    }
+    
+    func resetSamples() {
+        self.energySamples.removeAll()
+        self.distanceSamples.removeAll()
+        self.stepSamples.removeAll()
+        self.heartRateSamples.removeAll()
+    }
+
+    //MARK: HKWorkoutSessionDelegate
+    
+    func workoutSession(workoutSession: HKWorkoutSession, didChangeToState toState: HKWorkoutSessionState, fromState: HKWorkoutSessionState, date: NSDate) {
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            switch toState {
+            case .Running:
+                self.workoutDidStart(date)
+            case .Ended:
+                self.workoutDidEnd(date)
+            default:
+                print("Unexpected state \(toState)")
+            }
+        }
+    }
+    
+    func workoutSession(workoutSession: HKWorkoutSession, didFailWithError error: NSError) {
+        NSLog("Workout Session error : " + error.localizedDescription)
+    }
+    
+    //MARK: internal 
+    
+    func workoutDidStart(date: NSDate) {
+        self.workoutStartDate = date
+        
+        queries.append(self.createStreamingDistanceQuery(date))
+        queries.append(self.createStreamingEnergyQuery(date))
+//        queries.append(self.createStreamingStepQuery(date))
+        queries.append(self.createStreamingHeartRateQuery(date))
+        
+        for query in queries {
+            self.healthStore.executeQuery(query)
+        }
+        
+        self.delegate?.workoutSessionManager(self, didStartWorkoutWithDate: date)
+    }
+    
+    func workoutDidEnd(date: NSDate) {
+        self.workoutEndDate = date
+        
+        for query in queries {
+            self.healthStore.stopQuery(query)
+        }
+        
+        self.queries.removeAll()
+        
+        self.delegate?.workoutSessionManager(self, didStopWorkoutWithDate: date)
+        
+        self.saveWorkout()
+    }
+    
+    func saveWorkout() {
+        guard let startDate = self.workoutStartDate, endDate = self.workoutEndDate else { return }
+        
+        let workout = HKWorkout(activityType: self.workoutSession.activityType, startDate: startDate, endDate: endDate, duration: endDate.timeIntervalSinceDate(startDate), totalEnergyBurned: self.currentEnergyQuantity, totalDistance: self.currentDistanceQuantity, metadata: nil)
+        
+        var allSamples: [HKQuantitySample] = []
+        allSamples += self.energySamples
+        allSamples += self.distanceSamples
+        allSamples += self.heartRateSamples
+//        allSamples += self.stepSamples
+        
+        self.healthStore.saveObject(workout) { (success, error) -> Void in
+            if success && allSamples.count > 0 {
+                self.healthStore.addSamples(allSamples, toWorkout: workout, completion: { (success, error) -> Void in
+                    if success {
+                        self.resetSamples()
+                    }
+                })
+            }
+        }
+    }
+    
+    //MARK: Data queries
+    
+    func createStreamingDistanceQuery(workoutStartDate: NSDate) -> HKQuery {
+        let predicate = HKQuery.predicateForSamplesWithStartDate(workoutStartDate, endDate: nil, options: .None)
+        
+        let distanceQuery = HKAnchoredObjectQuery(type: self.distanceType, predicate: predicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, samples, deletedObjects, anchor, error) -> Void in
+            self.addDistanceSamples(samples)
+        }
+        
+        distanceQuery.updateHandler = { query, samples, deletedObjects, anchor, error in
+            self.addDistanceSamples(samples)
+        }
+        
+        return distanceQuery
+    }
+    
+    
+    func addDistanceSamples(samples: [HKSample]?) {
+        guard let distanceSamples = samples as? [HKQuantitySample] else { return }
+        
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+//            self.currentDistanceQuantity = self.currentDistanceQuantity.addQuantitiesFromSamples(distanceSamples, unit: self.distanceUnit)
+            self.distanceSamples += distanceSamples
+            
+            self.delegate?.workoutSessionManager(self, didUpdateDistanceQuantity: self.currentDistanceQuantity)
+        }
+    }
+    
+    func createStreamingStepQuery(workoutStartDate: NSDate) -> HKQuery {
+        let predicate = self.predicateFromWorkoutSamples(workoutStartDate)
+        
+        let distanceQuery = HKAnchoredObjectQuery(type: self.stepType, predicate: predicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, samples, deletedObjects, anchor, error) -> Void in
+            self.addStepSamples(samples)
+        }
+        
+        distanceQuery.updateHandler = { query, samples, deletedObjects, anchor, error in
+            self.addStepSamples(samples)
+        }
+        
+        return distanceQuery
+    }
+    
+    
+    func addStepSamples(samples: [HKSample]?) {
+        guard let stepSamples = samples as? [HKQuantitySample] else { return }
+        
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+//            self.currentStepQuantity = self.currentStepQuantity.addQuantitiesFromSamples(stepSamples, unit: self.stepUnit)
+            self.stepSamples += stepSamples
+            
+            self.delegate?.workoutSessionManager(self, didUpdateStepQuantity: self.currentStepQuantity)
+        }
+    }
+    
+    func createStreamingEnergyQuery(workoutStartDate: NSDate) -> HKQuery {
+        let predicate = self.predicateFromWorkoutSamples(workoutStartDate)
+        
+        // sum the new quantities with the current active energy quantity
+        let sampleHandler = { (samples: [HKQuantitySample]) -> Void in
+//            self.currentEnergyQuantity = self.currentEnergyQuantity.addQuantitiesFromSamples(samples, unit: self.distanceUnit)
+            
+            self.delegate?.workoutSessionManager(self, didUpdateEnergyQuantity: self.currentEnergyQuantity)
+        }
+        
+        let energyQuery = HKAnchoredObjectQuery(type: self.energyType, predicate: predicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, samples, deletedObjects, anchor, error) -> Void in
+            if let quantitySamples = samples as? [HKQuantitySample] {
+                sampleHandler(quantitySamples)
+            }
+        }
+        
+        energyQuery.updateHandler = { query, samples, deletedObjects, anchor, error in
+            if let quantitySamples = samples as? [HKQuantitySample] {
+                sampleHandler(quantitySamples)
+            }
+        }
+        
+        return energyQuery
+    }
+    
+    func createStreamingHeartRateQuery(workoutStartDate: NSDate) -> HKQuery {
+        let predicate = self.predicateFromWorkoutSamples(workoutStartDate)
+        
+        // sum the new quantities with the current active energy quantity
+        let sampleHandler = { (samples: [HKQuantitySample]) -> Void in
+            var mostRecentSample = self.currentHeartRateSample
+            var mostRecentStartDate = mostRecentSample?.startDate ?? NSDate.distantPast()
+            
+            for sample in samples {
+                if mostRecentStartDate.compare(sample.startDate) == .OrderedAscending {
+                    mostRecentSample = sample
+                    mostRecentStartDate = sample.startDate
+                }
+            }
+            
+            self.currentHeartRateSample = mostRecentSample
+            
+            if let sample = mostRecentSample {
+                self.delegate?.workoutSessionManager(self, didUpdateHeartRateSample: sample)
+            }
+        }
+        
+        let heartRateQuery = HKAnchoredObjectQuery(type: self.heartRateType, predicate: predicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, samples, deletedObjects, anchor, error) -> Void in
+            if let quantitySamples = samples as? [HKQuantitySample] {
+                sampleHandler(quantitySamples)
+            }
+        }
+        
+        heartRateQuery.updateHandler = { query, samples, deletedObjects, anchor, error in
+            if let quantitySamples = samples as? [HKQuantitySample] {
+                sampleHandler(quantitySamples)
+            }
+        }
+        
+        return heartRateQuery
+    }
+    
+    func predicateFromWorkoutSamples(startDate: NSDate) -> NSPredicate {
+        return HKQuery.predicateForSamplesWithStartDate(startDate, endDate: nil, options: .None)
+    }
+    
+}
+
